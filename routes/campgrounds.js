a3b79ab4d901aa129b9f1cff63d211cff1cf0509
Archish <archish500910@gmail.com>
2020-04-02 19:38:37 +0530

    Delete button added to show

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -84,6 +84,17 @@ router.put("/:id", (req, res) => {
     );
 });
 
+//DESTROY CAMPGROUND ROUTE
+router.delete("/:id", (req, res) => {
+    Campground.findByIdAndRemove(req.params.id, err => {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
